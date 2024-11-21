c8c3ef2702ddeb879a309c4072a32eb3aeedf6cf
Aystb <3185269857@qq.com>
2024-11-21 21:35:59 +0800

    feat/init

---
+++ coffee_flower_manage/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <view>
+    this is coffee-manage-flower-front
+  </view>
+</template>
+
+<style scoped>
+
+</style>
